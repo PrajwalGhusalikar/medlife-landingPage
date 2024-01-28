09ba7b3945645ba1f16120c805ec3e52f736d451
prajval_100 <prajwalghusalikar@gmail.com>
2024-01-28 15:25:33 +0530

    Fixed Home Page Animations

--- Frontend/src/components/Introduction.jsx
+++ Frontend/src/components/Introduction.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import sideImg from "../assets/sideimage.png";
-import { easeIn, easeInOut, motion } from "framer-motion";
-
+import { easeIn, easeInOut, easeOut, motion } from "framer-motion";
+import { Link } from "react-scroll";
 const Introduction = () => {
+  const easing = [0.6, -0.05, 0.01, 0.99];
+  const fadeInUp = {
+    initial: {
+      y: 60,
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+        ease: easing,
+      },
+    },
+
+    animate: {
+      y: 5,
+      opacity: 1,
+      transition: {
+        duration:2,
+        ease: easing,
+      },
+    },
+  };
+  const stagger = {
+    animate: {
+      transition: {
+        delayChildren: 0.4,
+        staggerChildren: 0.2,
+        staggerDirection: 1,
+      },
+    },
+  };
   return (
-    <div name="home">
+    <motion.div variants={stagger} initial="initial"  animate="animate" name="home">
       <motion.div
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -16,14 +45,25 @@ const Introduction = () => {
         </div>
         <div className="  w-screen sm:w-1/2 m-4 sm:mt-4 p-3 flex flex-col  items-center">
           <motion.div
-            initial={{opacity: 0, scale: 0 ,translateY:-20}} animate={{opacity: 1, scale: 1 ,translateY:0}}    transition={{ duration: 0.6, delay: 0.5, ease: easeInOut }}
+            initial={{ opacity: 0, scale: 0, translateY: -20 }}
+            animate={{ opacity: 1, scale: 1, translateY: 0 }}
+            transition={{ duration: 0.6, delay: 0.5, ease: easeInOut }}
             className="text-3xl sm:text-4xl text-center mb-2 sm:my-4 "
             style={{ color: "#554869" }}
           >
             Your Medical Companion, Anytime, Anywhere...
           </motion.div>
 
-          <motion.div initial={{opacity: 0.6 ,translateY:-20}} animate={{opacity: 1,translateY:0}}    transition={{ duration: 0.6, delay: 0.4, ease: easeInOut}} className="text-xl text-slate-500 my-2  text-center">
+          <motion.div
+            initial={{ opacity: 0.6, translateY: -20 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{
+              duration: 0.6,
+              delay: 0.4,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+            className="text-xl text-slate-500 my-2  text-center"
+          >
             <p className="">
               {" "}
               Revolutionizing healthcare through cutting-edge AI, tailored to
@@ -31,21 +71,33 @@ const Introduction = () => {
             </p>
           </motion.div>
           <motion.div
-          initial={{opacity: 0 ,translateY:40}} animate={{opacity: 1,translateY:0}} transition={{duration:0.6,delay:0.8,  ease: easeIn }}
+           variants={fadeInUp}
+            whileHover={{
+              scale: 1.1,
+              transition: { duration: 0.1, delay: 0, ease:easeOut },
+            }}
             className="text-base w-72 h-16 rounded-2xl text-center m-auto mt-4 p-2  shadow-custom transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-emerald-100  duration-150 cursor-pointer   "
             style={{
               background:
                 "linear-gradient(to bottom right, #64deb2 0%, #b1f9d8 100%)",
             }}
           >
-            Choose Your Plan & Empower Your Health Journey
+            <Link
+              to="pricing"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Choose Your Plan & Empower Your Health Journey
+            </Link>
           </motion.div>
         </div>
         <div className="  m-3 hidden sm:block h-[500] w-[500] ">
           <img src={sideImg} alt="" className="p-2 " />
         </div>
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
